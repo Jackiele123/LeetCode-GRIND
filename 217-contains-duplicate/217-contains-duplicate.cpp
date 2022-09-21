9c77f58592ada63c0355ad70b187d369f2744af2
Jackiele123 <43102053+Jackiele123@users.noreply.github.com>
2022-09-20 21:50:57 -0400

    Time: 218 ms (17.25%), Space: 52.2 MB (35.56%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+    
+        map<int,bool> map; 
+        for (int i =0; i < nums.size(); i++){
+            if (map.count(nums[i]) == true)
+                return true;
+            map[nums[i]] = true;
+        
+        }
+    return false;
+    }
+};
